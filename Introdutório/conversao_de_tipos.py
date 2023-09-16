2008bf5772b248f40e3260851fed8a9ff93ca7e3
RicS1lva <ricardo.ssk2@gmail.com>
2023-09-15 23:22:09 -0300

    Aula de Conversão de tipos

---
+++ Introdutório/conversao_de_tipos.py
@@ -0,0 +1,60 @@
+#Int para Float
+price = 10
+print(price)
+# 10
+
+price = float(price)
+print(price)
+# 10.0
+
+price = 10 / 2
+print(price)
+# 5.0
+
+# Float para Int
+
+price = 10.30
+print(price)
+# 10.30
+
+price = int(price)
+print(price)
+# 10
+
+# Conversão por divisão
+
+price = 10
+print(price)
+# 10
+
+print(price / 2)
+# 5.0
+
+print(price // 2)
+# 5 (nesta conversão com duas barras, ele conserva o int
+
+# Numérico para string
+
+preco = 10.50
+idade = 25
+
+print(str(preco))
+# 10.5
+
+print(str(idade))
+# 25
+
+texto = f'idade {idade} preco {preco}'
+print(texto)
+# idade 25 preco 10.50
+
+# String para número
+
+preco = "10.50"
+idade = "25"
+
+print(float(preco))
+# 10.50
+
+print(int(idade))
+# 25
