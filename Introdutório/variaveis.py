7f8184b896ed3a007a6c9fbb011c03e89238c4a8
RicS1lva <ricardo.ssk2@gmail.com>
2023-09-15 22:07:31 -0300

    Aula de variáveis e constantes e arquivo README.md

---
+++ Introdutório/variaveis.py
@@ -0,0 +1,14 @@
+age = 25
+name = "Ricardo"
+print(f' Meu nome é {name}, e eu tenho {age} anos de idade.')
+# Meu nome é Ricardo, e eu tenho 25 anos de idade.
+
+age, name = (25, "Ricardo")
+print(f' Meu nome é {name}, e eu tenho {age} anos de idade.')
+# Meu nome é Ricardo, e eu tenho 25 anos de idade.
+
+age = 28
+name = "Rafael"
+print(f' Meu nome é {name}, e eu tenho {age} anos de idade.')
+# Meu nome é Ricardo, e eu tenho 25 anos de idade.
+# Para alterar os valores de forma básica, basta atribuir novos valores à variável.
---
+++ Introdutório/variaveis_constantes.py
@@ -0,0 +1,16 @@
+name = 'Ricardo'
+age = 25
+
+print(name, age)
+
+name, age = 'Fernanda', 33
+
+print(name, age)
+
+limite_saque_diario = 1000 #snake case
+LimiteSaqueDiario = 1000 #camel case
+lim_saq_di = 1000 # mau exemplo de variável
+
+BRAZILIAN_STATES = ['SP', 'RJ' 'SC', 'RS'] # constante, tudo em maiúsculo
+
+print(BRAZILIAN_STATES)
