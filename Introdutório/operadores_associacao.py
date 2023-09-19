e3b4dc59fe71b46c1192378e247235071f49998c
RicS1lva <ricardo.ssk2@gmail.com>
2023-09-19 18:16:27 -0300

    Operadores pt.2

---
+++ Introdutório/operadores_associacao.py
@@ -0,0 +1,11 @@
+frutas = ["limão", "uva", "laranja"]
+curso = "Curso de Python"
+
+
+print("laranja" in frutas)
+print("Laranja" in frutas)
+print("maça" in frutas)
+print("limão" not in frutas)
+print("laranja" not in frutas)
+print("")
+print("Python" in curso)
