32af47b3604cca794ed9c91c8a563d855cd3e901
RicS1lva <ricardo.ssk2@gmail.com>
2023-09-26 22:06:44 -0300

    Manipulando Strings

---
+++ Introdutório/string_pt2.py
@@ -0,0 +1,23 @@
+nome = "Guilherme"
+idade = 28
+profissao = "Programador"
+linguagem = "Python"
+saldo = 45.435
+
+dados = {"nome": "Guilherme", "idade": 28}
+
+print("Nome: %s Idade: %d" % (nome, idade))
+print("")
+print("Nome: {} Idade: {}".format(nome, idade))
+print("")
+print("Nome: {1} Idade: {0}".format(idade, nome))
+print("Nome: {1} Idade: {0} Nome: {1}".format(idade, nome))
+print("")
+print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
+print("Nome: {name} Idade: {age} {name} {name} {age}".format(age=idade, name=nome))
+print("Nome: {nome} Idade: {idade}".format(**dados))
+print("")
+print(f"Nome: {nome} Idade: {idade}")
+print(f"Nome: {nome} Idade: {idade} saldo: {saldo:.2f}")
+print(f"Nome: {nome} Idade: {idade} saldo: {saldo:6.2f}")
+print(f"Nome: {nome} Idade: {idade} saldo: {saldo:10.2f}")
---
+++ Introdutório/string_pt4.py
@@ -0,0 +1,27 @@
+nome = "Guilherme"
+
+mensagem = f'''
+	Olá meu nome é {nome},
+ Eu estou aprendendo Python
+		Essa mensagem tem diferentes recuos.
+'''
+
+print(mensagem)
+
+print('''
+    ============= MENU =============
+      
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+      
+    ================================
+      
+            Obrigado por usar nosso sistema!!!!
+''')
+
+menu = "============= MENU ============="
+menu += "\n"
+menu += "1 - Depositar\n"
+
+print(menu)
